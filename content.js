bf756aee0c820a142576349778ee65f1f5523e7e
Hiuoy <119734912+Hiuoy@users.noreply.github.com>
2026-09-19 20:48:22 +0400

    fixes v1.2

--- content.js
+++ content.js
@@ -1,14 +1,13 @@
-document.addEventListener('auxclick', function(event) {
-
+window.addEventListener('auxclick', function(event) {
   if (event.button === 1) {
     const selectedText = window.getSelection().toString().trim();
     
-    
     if (selectedText.length > 0) {
-      
       event.preventDefault();
       
-      chrome.runtime.sendMessage({ type: "SEARCH_TEXT", text: selectedText });
+      const searchUrl = "https://www.google.com/search?q=" + encodeURIComponent(selectedText);
+      
+      window.open(searchUrl, '_blank');
     }
   }
-});
+}, true); 
